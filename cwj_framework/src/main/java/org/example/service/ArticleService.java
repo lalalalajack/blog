69e99226a6e02248e79ad320d24ef7e49ea3354a
lalalalajack <1875576725@qq.com>
2023-06-02 13:01:22 +0800

    分页查询文章列表及fastJson配置

--- cwj_framework/src/main/java/org/example/service/ArticleService.java
+++ cwj_framework/src/main/java/org/example/service/ArticleService.java
@@ -13,5 +13,7 @@ import org.example.domain.entity.Article;
 public interface ArticleService extends IService<Article> {
 
     ResponseResult hotArticleList();
+
+    ResponseResult articleList(Integer pageNum, Integer pageSize, Long categoryId);
 }
 
