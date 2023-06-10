dcd39d413874631cb0fbff7c7ab485909323f436
lalalalajack <1875576725@qq.com>
2023-06-10 21:27:41 +0800

    【博客后台】分类管理CRUD

--- admin/src/main/java/org/example/controller/CategoryController.java
+++ admin/src/main/java/org/example/controller/CategoryController.java
@@ -3,12 +3,12 @@ package org.example.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.example.domain.ResponseResult;
+import org.example.domain.entity.Category;
 import org.example.domain.vo.CategoryVo;
+import org.example.domain.vo.PageVo;
 import org.example.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +30,63 @@ public class CategoryController {
         return ResponseResult.okResult(list);
     }
 
+
+    /**
+     * 分页查询分类列表
+     * @param category
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/list")
+    public ResponseResult list(Category category, Integer pageNum, Integer pageSize) {
+        //TODO 将Category 转为DTO
+        PageVo pageVo = categoryService.selectCategoryPage(category,pageNum,pageSize);
+        return ResponseResult.okResult(pageVo);
+    }
+
+    /**
+     * 根据id获取分类信息
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/{id}")
+    public ResponseResult getInfo(@PathVariable(value = "id")Long id){
+        Category category = categoryService.getById(id);
+        return ResponseResult.okResult(category);
+    }
+
+    /**
+     * 新增分类
+     * @param category
+     * @return
+     */
+    @PostMapping
+    public ResponseResult add(@RequestBody Category category){
+        categoryService.save(category);
+        return ResponseResult.okResult();
+    }
+
+    /**
+     * 修改分类
+     * @param category
+     * @return
+     */
+    @PutMapping
+    public ResponseResult edit(@RequestBody Category category){
+        categoryService.updateById(category);
+        return ResponseResult.okResult();
+    }
+
+    /**
+     * 删除分类
+     * @param id
+     * @return
+     */
+    @DeleteMapping(value = "/{id}")
+    public ResponseResult remove(@PathVariable(value = "id")Long id){
+        categoryService.removeById(id);
+        return ResponseResult.okResult();
+    }
 
 }
