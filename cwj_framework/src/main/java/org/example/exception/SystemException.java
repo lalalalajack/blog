bbfb369bde5962622201a0b0f1995132fb4efaf7
lalalalajack <1875576725@qq.com>
2023-11-05 21:43:11 +0800

    统一异常处理

---
+++ cwj_framework/src/main/java/org/example/exception/SystemException.java
@@ -0,0 +1,27 @@
+package org.example.exception;
+
+import org.example.enums.AppHttpCodeEnum;
+
+/**
+ * 自定义系统异常类
+ */
+public class SystemException extends RuntimeException{
+    private int code;
+
+    private String msg;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public SystemException(AppHttpCodeEnum httpCodeEnum) {
+        super(httpCodeEnum.getMsg());
+        this.code = httpCodeEnum.getCode();
+        this.msg = httpCodeEnum.getMsg();
+    }
+
+}
