6bf68dc515d6bb9e6f5650368c7757a134ac60fd
lalalalajack <1875576725@qq.com>
2023-11-10 18:53:51 +0800

    【博客后台】标签列表CRUD

--- cwj_framework/src/main/java/org/example/domain/entity/Tag.java
+++ cwj_framework/src/main/java/org/example/domain/entity/Tag.java
@@ -2,6 +2,8 @@ package org.example.domain.entity;
 
 import java.util.Date;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
@@ -26,12 +28,16 @@ public class Tag {
     //标签名
     private String name;
 
+    @TableField(fill = FieldFill.INSERT)
     private Long createBy;
 
+    @TableField(fill = FieldFill.INSERT)
     private Date createTime;
 
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Long updateBy;
 
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
     //删除标志（0代表未删除，1代表已删除）
     private Integer delFlag;
