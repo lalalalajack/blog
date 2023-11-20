b623b8bcc867268ee7240e5ba213ec4db1517004
lalalalajack <1875576725@qq.com>
2023-11-20 22:18:23 +0800

    更替为fastjson

--- cwj_framework/src/main/java/org/example/config/WebConfig.java
+++ cwj_framework/src/main/java/org/example/config/WebConfig.java
@@ -1,11 +1,12 @@
 package org.example.config;
 
-//import com.alibaba.fastjson.serializer.SerializeConfig;
-//import com.alibaba.fastjson.serializer.SerializerFeature;
-//import com.alibaba.fastjson.serializer.ToStringSerializer;
-//import com.alibaba.fastjson.support.config.FastJsonConfig;
-//import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.serializer.ToStringSerializer;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+//import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -41,50 +42,50 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(3600);
     }
 
-//    /**
-//     * fastJson配置类
-//     * @return
-//     */
-//    @Bean//使用@Bean注入fastJsonHttpMessageConvert
-//    public HttpMessageConverter fastJsonHttpMessageConverters() {
-//        //1.需要定义一个Convert转换消息的对象
-//        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-//
-//        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-//        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-//        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
-//
-//        SerializeConfig.globalInstance.put(Long.class, ToStringSerializer.instance);
-//        fastJsonConfig.setSerializeConfig(SerializeConfig.globalInstance);
-//
-//        fastConverter.setFastJsonConfig(fastJsonConfig);
-//        HttpMessageConverter<?> converter = fastConverter;
-//        return converter;
-//    }
+    /**
+     * fastJson配置类
+     * @return
+     */
+    @Bean//使用@Bean注入fastJsonHttpMessageConvert
+    public HttpMessageConverter fastJsonHttpMessageConverters() {
+        //1.需要定义一个Convert转换消息的对象
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
 
-//    /**
-//     * 重写 配置消息转换器类
-//     * @param converters
-//     */
-//    @Override
-//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-//        converters.add(fastJsonHttpMessageConverters());
-//    }
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        SerializeConfig.globalInstance.put(Long.class, ToStringSerializer.instance);
+        fastJsonConfig.setSerializeConfig(SerializeConfig.globalInstance);
+
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fastConverter;
+        return converter;
+    }
+
+    /**
+     * 重写 配置消息转换器类
+     * @param converters
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(fastJsonHttpMessageConverters());
+    }
 
     /**
      * Jackson全局转化long类型为String，解决jackson序列化时long类型缺失精度问题
      * @return Jackson2ObjectMapperBuilderCustomizer 注入的对象
      */
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
-
-        return jacksonObjectMapperBuilder -> {
-            jacksonObjectMapperBuilder.serializerByType(Long.TYPE, ToStringSerializer.instance);
-            jacksonObjectMapperBuilder.serializerByType(Long.class, ToStringSerializer.instance);
-            jacksonObjectMapperBuilder.serializerByType(BigInteger.class, ToStringSerializer.instance);
-            jacksonObjectMapperBuilder.serializerByType(BigDecimal.class, ToStringSerializer.instance);
-        };
-    }
+//    @Bean
+//    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+//
+//        return jacksonObjectMapperBuilder -> {
+//            jacksonObjectMapperBuilder.serializerByType(Long.TYPE, ToStringSerializer.instance);
+//            jacksonObjectMapperBuilder.serializerByType(Long.class, ToStringSerializer.instance);
+//            jacksonObjectMapperBuilder.serializerByType(BigInteger.class, ToStringSerializer.instance);
+//            jacksonObjectMapperBuilder.serializerByType(BigDecimal.class, ToStringSerializer.instance);
+//        };
+//    }
 
 
 }
